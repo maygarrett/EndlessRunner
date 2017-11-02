f0e7506758b7b01dac48d4d25c7871fb21670f60
Garrett May <maygarrett@hotmail.com>
2017-11-02 11:58:14 -0400

    EDIT - Small changes to Constants Script

--- Endless Runner/Assets/Scripts/Constants.cs
+++ Endless Runner/Assets/Scripts/Constants.cs
@@ -1,40 +1,51 @@
-﻿using System;
+﻿// Endless Runner Constants Script
+
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace GameData {
-	public static class Constants {
-		private static Dictionary<string,int> intConstants = new Dictionary<string, int>();
+namespace GameData
+{
+    public static class Constants
+    {
+        private static Dictionary<string, int> intConstants = new Dictionary<string, int>();
         private static Dictionary<string, float> floatConstants = new Dictionary<string, float>();
         private static Dictionary<string, string> stringConstants = new Dictionary<string, string>();
 
-        public static void Initialize() {
-			TextAsset constantsCSV = Resources.Load<TextAsset>("Constants");
+        public static void Initialize()
+        {
+            TextAsset constantsCSV = Resources.Load<TextAsset>("Constants");
 
-			string[] lines = constantsCSV.text.Split('\n');
+            string[] lines = constantsCSV.text.Split('\n');
 
-			foreach(string l in lines) {
-				string[] data = l.Split(',');
+            foreach (string l in lines)
+            {
+                string[] data = l.Split(',');
 
-				if(data.Length == 3) {
-					string key = data[0];
-					string type = data[1];
-					string value = data[2];
+                if (data.Length == 3)
+                {
+                    string key = data[0];
+                    string type = data[1];
+                    string value = data[2];
 
-					if(type == "INT") {
-						int v;
 
-						if(int.TryParse(value,out v)) {
-							intConstants.Add(key,v);
-						} else {
+                    if (type == "INT")
+                    {
+                        int v;
+
+                        if (int.TryParse(value, out v))
+                        {
+                            intConstants.Add(key, v);
+                        }
+                        else
+                        {
                             Debug.LogError("Tried to add something that wasn't an INT into the INT constants array");
-						}
-					}
+                        }
+                    }
 
                     if (type == "FLOAT")
                     {
                         float v;
-
                         if (float.TryParse(value, out v))
                         {
                             floatConstants.Add(key, v);
@@ -47,15 +58,16 @@ namespace GameData {
 
                     if (type == "STRING")
                     {
-                            stringConstants.Add(key, value);
+                        stringConstants.Add(key, value);
                     }
                 }
-			}
-		}
+            }
+        }
 
-		public static int GetInt(string key) {
-			return intConstants[key];
-		}
+        public static int GetInt(string key)
+        {
+            return intConstants[key];
+        }
 
         public static float GetFloat(string key)
         {
