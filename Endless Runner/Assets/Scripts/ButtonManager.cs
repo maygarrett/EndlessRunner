01678c3be6288c0c634c2275fcef0b0286cba748
Garrett May <maygarrett@hotmail.com>
2017-11-09 12:44:04 -0500

    UPDATE - Added Localized text to all main menu buttons in main menu scene

--- Endless Runner/Assets/Scripts/ButtonManager.cs
+++ Endless Runner/Assets/Scripts/ButtonManager.cs
@@ -8,11 +8,21 @@ public class ButtonManager : MonoBehaviour {
 
     [SerializeField]
     private Text _START_GAME;
+    [SerializeField]
+    private Text _OPTIONS_MAIN_MENU;
+    [SerializeField]
+    private Text _INSTRUCTIONS_MAIN_MENU;
+    [SerializeField]
+    private Text _CREDITS_MAIN_MENU;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        // assign localized texts
         _START_GAME.text = Localization.GetString("START_GAME");
-	}
+        _OPTIONS_MAIN_MENU.text = Localization.GetString("MAIN_MENU");
+        _INSTRUCTIONS_MAIN_MENU.text = Localization.GetString("MAIN_MENU");
+        _CREDITS_MAIN_MENU.text = Localization.GetString("MAIN_MENU");
+    }
 	
 	// Update is called once per frame
 	void Update () {
