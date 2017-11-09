b4e0d50dd03cf6641115c4e82463f988bd4bbd76
Garrett May <maygarrett@hotmail.com>
2017-11-09 12:23:49 -0500

    UPDATE - Added localization script, localization manager to handle initializing of localization csv file

---
+++ Endless Runner/Assets/Scripts/Localization.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameData
+{
+    public static class Localization
+    {
+        private static Dictionary<string, string> localizationDictionary = new Dictionary<string, string>();
+
+        public static void Initialize()
+        {
+            TextAsset localizationCSV = Resources.Load<TextAsset>("Localization");
+
+            string[] lines = localizationCSV.text.Split('\n');
+
+            foreach (string l in lines)
+            {
+                string[] data = l.Split(',');
+
+                if (data.Length == 6)
+                {
+                    string key = data[0];
+                    string type = data[1];
+                    string englishValue = data[2];
+                    string frenchValue = data[3];
+                    string germanValue = data[4];
+                    string spanishValue = data[5];
+
+                    if (type == "STRING")
+                    {
+                        switch (Application.systemLanguage.ToString())
+                        {
+                            case "English":
+                                localizationDictionary.Add(key, englishValue);
+                                break;
+                            case "French":
+                                localizationDictionary.Add(key, frenchValue);
+                                break;
+                            case "German":
+                                localizationDictionary.Add(key, germanValue);
+                                break;
+                            case "Spanish":
+                                localizationDictionary.Add(key, spanishValue);
+                                break;
+                            default:
+                                Debug.LogError("Language Not Found, Defaulting to English");
+                                localizationDictionary.Add(key, englishValue);
+                                break;
+
+                        }
+                    }
+                }
+            }
+        }
+
+        public static string GetString(string key)
+        {
+            return localizationDictionary[key];
+        }
+    }
+}
+
---
+++ Endless Runner/Assets/Scripts/LocalizationManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameData;
+
+public class LocalizationManager : MonoBehaviour {
+
+    private static LocalizationManager _instance = null;
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            DestroyImmediate(gameObject);
+        }
+        else
+        {
+            instance = this;
+
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    private void Start()
+    {
+        //Constants.Initialize();
+        Localization.Initialize();
+    }
+
+    private static LocalizationManager instance
+    {
+        get { return _instance; }
+        set { _instance = value; }
+    }
+
+}
